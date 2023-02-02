ec80257a4ea941a00f7189045a3bec301a6f8c87
NicoleLima0 <nick01tererj@gmail.com>
2023-02-02 19:45:45 -0300

    Condição Simples If

--- Aula11/ex008.js
+++ Aula11/ex008.js
@@ -1,9 +1,9 @@
 var vel = 50
 console.log(`A velocidade do seu carro é de ${vel}Km/h`)
-if (vel>60) {
+if (vel>60) { // condição simples
     console.log('Você foi multado')
 } {
-    console.log('Tenha uma boa viagem!')
+    console.log('Tenha uma boa viagem!')//tem que executar com o node exec, tecla F8
 }
 
 
