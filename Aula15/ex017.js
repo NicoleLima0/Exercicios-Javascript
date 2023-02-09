624359f8d9049b2f6ba1f8caffa335559a9f1860
NicoleLima0 <nick01tererj@gmail.com>
2023-02-09 10:10:38 -0300

    Vetores

---
+++ Aula15/ex017.js
@@ -0,0 +1,5 @@
+var num = [9,1,7]
+num.push(4)
+console.log (`${num.sort()}`)
+console.log (`${num.length}`)
+console.log (`${num[3]}`)
---
+++ Aula15/vetornatela.js
@@ -0,0 +1,12 @@
+var num = [7,8,0,6,5]
+console.log ( num.indexOf(6))//mostra em qual posição meu elemento está
+
+/*for (var pos in num){
+    console.log (`${num[pos]}`)
+}
+*/
+
+/*for (var pos = 0; pos<num.length;pos++){
+    console.log (`${num[pos]}`)
+}
+*/
